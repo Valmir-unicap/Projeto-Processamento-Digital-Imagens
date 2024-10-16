027f77068147daf4e1046968adf63a7e93141bb4
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2024-10-16 12:32:37 -0300

    Adicionando comentário para passos do Laplacian.py

--- Algoritmos/Laplacian.py
+++ Algoritmos/Laplacian.py
@@ -4,6 +4,7 @@ from google.colab.patches import cv2_imshow
 import cv2
 import numpy as np
 
+# 1. Carregar a Imagem
 image_path = '/content/lena.png'  
 image = cv2.imread(image_path)
 
@@ -12,6 +13,7 @@ if image is None:
     print(f"Error: Could not load image from {image_path}. Please check the file path and ensure the image exists.")
 else:
 
+     # 2. Converter para Escala de Cinza (com borrão)
     image_blur = cv2.GaussianBlur(image, (3, 3), 0)
 
     imagegray = cv2.cvtColor(image_blur, cv2.COLOR_BGR2GRAY)
@@ -19,7 +21,11 @@ else:
     cv2_imshow(image)
     cv2_imshow(imagegray)
 
+    # 3. Definir e aplicar o filtro Laplaciano
+    # 4. Tratar as Bordas (já tratado na aplicação do filtro)
     lap = cv2.Laplacian(imagegray, cv2.CV_64F)
     cv2_imshow(lap)
+
+     # 6. Normalização da imagem
     lap2 = np.uint8(np.absolute(lap))
     cv2_imshow(lap2) 
